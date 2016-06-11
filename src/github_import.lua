e283e47ab52b0f262650d131ac80668730624af4
Nils <nilsgawlik@gmx.de>
2016-06-11 17:48:07 +0200

    asdf

--- src/github_import.lua
+++ src/github_import.lua
@@ -1,5 +1,6 @@
 names = {"automine", "turtle_utils" }
 
 for i, name in ipairs(names) do
+  shell.run("delete", name)
   shell.run("github", "nilsgawlik/ComputerCraftPrograms/master/src/" .. name .. ".lua", name)
 end
--- src/github_import_import.lua
+++ src/github_import_import.lua
@@ -1,1 +1,2 @@
+shell.run("delete", "github_import")
 shell.run("github", "nilsgawlik/ComputerCraftPrograms/master/src/github_import.lua", "github_import")
