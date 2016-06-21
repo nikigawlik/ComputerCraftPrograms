ea6c7de3f59d5408ca0818a7bc6a3c968fd2854b
Nils <nilsgawlik@gmx.de>
2016-06-21 17:23:34 +0200

    added turtlesquad and updated turtle_utils

--- src/turtle_utils.lua
+++ src/turtle_utils.lua
@@ -1,3 +1,31 @@
+function turtle.checkAndRefuel(level, slot)
+    if turtle.getFuelLevel() < level then
+      if slot then
+        turtle.select(slot)
+        if turtle.refuel(0)  then
+          while turtle.getFuelLevel() < level do
+            turtle.refuel(1)
+          end
+          return
+        end
+      end
+      for i=1,16 do
+        turtle.select(i)
+        if turtle.refuel(0) then
+          while turtle.getFuelLevel() < level do
+            turtle.refuel(1)
+          end
+          return
+        end
+      end
+    end
+end
+
+function turtle.reverse()
+  turtle.turnLeft()
+  turtle.turnLeft()
+end
+
 function turtle.carefulDig()
     repeat until not turtle.dig()
 end
@@ -6,4 +34,78 @@ function turtle.carefulDigUp()
 end
 function turtle.carefulDigDown()
     turtle.digDown()
+end
+
+function turtle.carefulForward() 
+  while not turtle.forward() do 
+    turtle.attack()
+    turtle.dig()
+  end
+end
+
+function turtle.carefulBack() 
+  if not turtle.back() then
+    turtle.reverse()
+    turtle.carefulForward()
+    turtle.reverse()
+  end
+end
+
+function turtle.carefulUp() 
+  while not turtle.up() do 
+    turtle.attackUp()
+    turtle.digUp()
+  end
+end
+
+function turtle.carefulDown() 
+  while not turtle.down() do 
+    turtle.attackDown()
+    turtle.digDown()
+  end
+end
+
+function turtle.mF(steps) 
+  for i=1,steps do
+    turtle.carefulForward()
+  end
+end
+function turtle.mD(steps) 
+  for i=1,steps do
+    turtle.carefulDown()
+  end
+end
+function turtle.mU(steps) 
+  for i=1,steps do
+    turtle.carefulUp()
+  end
+end
+
+function turtle.selectItem(name)
+  local startslot = turtle.getSelectedSlot()
+  for i = 0,15 do
+    turtle.select(((startslot+i - 1) % 16 + 1))
+    item = turtle.getItemDetail()
+    if item then
+      if item.name == name or "minecraft:" .. item.name == name then
+        break
+      end
+    end
+  end
+end 
+
+function turtle.carefulPlaceDown() 
+  while not turtle.placeDown() do
+    turtle.digDown()
+  end
+end
+function turtle.carefulPlaceUp() 
+  while not turtle.placeUp() do
+    turtle.digUp()
+  end
+end
+function turtle.carefulPlace() 
+  while not turtle.place() do
+    turtle.dig()
+  end
 end
---
+++ src/turtlesquad.lua
@@ -0,0 +1,56 @@
+args = {...}
+
+shell.run("turtle_utils")
+
+-- turtle faces "negative y" at beginning 
+-- in relation to x and y below
+
+if #args ~= 4 then
+  print("usage: turtlesquad w h dx dy coalAmount")
+  exit()
+end
+
+w = args[1]
+h = args[2]
+dx = args[3]
+dy = args[4]
+coalAmount = args[5]
+
+function placeCopyReboot()
+  --  TODO
+  turtle.selectItem("ComputerCraft:CC-TurtleExpanded")
+  turtle.place()
+  turtle.mF(1)
+  turtle.selectItem("ComputerCraft:CC-Peripheral")
+  turtle.carefulPlaceDown()
+  turtle.selectItem("ComputerCraft:diskExpanded")
+  turtle.dropDown()
+  turtle.carefulBack()
+  local inst = peripheral.wrap("down")
+  inst.reboot()
+  turtle.carefulForward()
+  turtle.suckDown()
+  turtle.digDown()
+  turtle.carefulBack()
+  turtle.selectItem("minecraft:coal")
+  turtle.dropDown(coalAmount)
+  inst.reboot()
+end
+
+goingForw = true
+
+for y=1,w do
+  for x=1,h do
+    placeCopyReboot()
+    turtle.mF(dx)
+  end
+  if goingForw then
+    turtle.turnLeft()
+    turtle.mF(1)
+    turtle.turnLeft()
+  else
+    turtle.turnRight()
+    turtle.mF(1)
+    turtle.turnRight()
+  end
+end
